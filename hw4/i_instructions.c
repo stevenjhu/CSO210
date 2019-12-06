47d75d275f40a08d6eb68430a6a542c954d102ce
stevenjhu <s19990922@gmail.com>
2019-12-06 11:48:04 -0500

    hw4m_dec6_11

--- hw4/i_instructions.c
+++ hw4/i_instructions.c
@@ -7,55 +7,58 @@
 #define IMD(x)		x&0xFFFF
 
 void addi(unsigned int instruction){
-	unsigned int test = (registers[IMD(instruction)]>>15)&0x1;
-	if (test != 0x0){
-		unsigned int sign_extended = IMD(instruction)|(0xFFFF<<16)
-		registers[RT(instruction)]=(unsigned int)((int)registers[RS(instruction)]+(int)sign_extended);
-	}else{
-		registers[RT(instruction)]=(unsigned int)((int)registers[RS(instruction)]+(int)IMD(instruction));
-	}
+	registers[RT(instruction)]=(unsigned int)((int)registers[RS(instruction)]+(int)sign_extend(IMD(instruction),16));
 }
 
 void addiu(unsigned int instruction){
-	unsigned int test = (registers[IMD(instruction)]>>15)&0x1;
-	if (test != 0x0){
-		unsigned int sign_extended = IMD(instruction)|(0xFFFF<<16)
-		registers[RT(instruction)]=registers[RS(instruction)]+sign_extended;
-	}else{
-		registers[RT(instruction)]=registers[RS(instruction)]+IMD(instruction);
-	}
+	registers[RT(instruction)]=registers[RS(instruction)]+sign_extend(IMD(instruction),16);
 }
 
 void andi(unsigned int instruction){
-
+	registers[RT(instruction)] = registers[RS(instruction)]&IMD(instruction);
+	//no need to make sure upper 16 bits are zero because (0 & anything) is 0
 }
 
 void ori(unsigned int instruction){
-
+	registers[RT(instruction)]=(registers[RS(instruction)]|IMD(instruction)<<16)>>16;
+	//make sure the upper 16 bits are zero
 }
 
 void xori(unsigned int instruction){
-
+	registers[RT(instruction)]=(registers[RS(instruction)]^IMD(instruction)<<16)>>16;
+		//make sure the upper 16 bits are zero
 }
 
 void slti(unsigned int instruction){
-
+	if((int)registers[RS(instruction)]<(int)sign_extend(IMD(instruction),16)){
+		registers[RT(instruction)]=1;
+	}else{
+		registers[RT(instruction)]=0;
+	}
 }
 
 void sltiu(unsigned int instruction){
-
+	if(registers[RS(instruction)]<sign_extend(IMD(instruction),16)){
+		registers[RT(instruction)]=1;
+	}else{
+		registers[RT(instruction)]=0;
+	}
 }
 
 void beq(unsigned int instruction){
-
+	if(registers[RS(instruction)]==registers[RT(instruction)]){
+		npc= pc+4+(sign_extend(IMD(instruction),16)<<2)
+	}
 }
 
 void bne(unsigned int instruction){
-
+	if(registers[RS(instruction)]!=registers[RT(instruction)]){
+		npc= pc+4+(sign_extend(IMD(instruction),16)<<2)
+	}
 }
 
-void lb(unsigned int instruction){
-
+void lb(unsigned int instruction){//check
+	registers[RT(instruction)]=sign_extend(memory[(int)registers[RS(instruction)]+(int)sign_extend(IMD(instruction),16)],8);
 }
 
 void lbu(unsigned int instruction){
@@ -88,4 +91,15 @@ void sh(unsigned int instruction){
 
 void sw(unsigned int instruction){
 
+}
+unsigned int sign_extend(unsigned int immediate,int bits){
+	unsigned int result = 0x0;
+	unsigned int test = (immediate>>(bits-1))&0x1;
+	if (test != 0x0){
+		unsigned int sign_extended = immediate|(0xFFFF<<16)
+		result = sign_extended;
+	}else{
+		result = immediate;
+	}
+	return result;
 }
